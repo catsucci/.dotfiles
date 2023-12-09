83813c71aeedb0398e4071df6aa69db7b07c353d
catsucci <150689455+catsucci@users.noreply.github.com>
2023-12-09 19:34:42 +0100

    Comment git init line
    Since we might already have an existing local git repo we might
    want to create a remote repo and linking it to the local one.

--- scripts/setup-repo.sh
+++ scripts/setup-repo.sh
@@ -11,6 +11,6 @@ CURRENT_DIR=$PWD
 PROJECT_DIR=${2:-$CURRENT_DIR}
 # GitHub repos Create API call
 curl -H "Authorization: token $GH_API_TOKEN" https://api.github.com/user/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}'
-git init $PROJECT_DIR
+#git init $PROJECT_DIR
 # Initialize Git in project directory, and add the GH repo remote.
 git -C $PROJECT_DIR remote add origin git@github.com:$GH_USER/$NEW_REPO_NAME.git
