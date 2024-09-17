7600de59c3ce0d061a4abc92737b71bede78342b
Lunar Haze <150689455+lunarhaze@users.noreply.github.com>
2024-09-17 17:18:10 +0100

    patch: scripts in  from now on

--- scripts/shell-cht.sh
+++ scripts/shell-cht.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Create a new window in the existing tmux session and run the command
-tmux new-window "bash -c '~/bin/.local/scripts/cht.sh --shell; exec \$SHELL'"
+tmux new-window "bash -c 'cht.sh --shell; exec \$SHELL'"
 
--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
 	selected=$1
 else
-	selected=$(find ~/Desktop/ ~/Dev/ ~/Lab/ ~/Documents/ ~/Documents/Reports/ ~/Repos/ -mindepth 1 -maxdepth 1 -type d | fzf)
+	selected=$(find ~/Desktop/ /mnt/c/Users/LunarHaze/Desktop /mnt/c/Dev /mnt/c/Lab ~/Dev/ ~/Lab/ ~/Documents/ ~/Documents/Reports/ ~/Repos/ -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
